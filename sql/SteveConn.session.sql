c9faff847456be2351917e132a273295b4feff91
Steve Robinett <steven.robinett@gfcmsu.edu>
2023-02-15 10:50:16 -0700

    add sql dir

---
+++ sql/SteveConn.session.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customer (
+    cust_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cust_fname VARCHAR(40) NOT NULL,
+    cust_lname VARCHAR(40) NOT NULL,
+    cust_email VARCHAR(60) NOT NULL
+);
+    
