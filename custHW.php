2b055891b86db46b7ac0df48c1b74596a6db3570
Jordyn Merchant <jo.merchant96@gmail.com>
2023-02-18 17:46:46 -0700

    homework

--- custHW.php
+++ custHW.php
@@ -8,10 +8,10 @@
 </head>
 <body>
     <?php 
-         $servername = "localhost";
-         $username = "steverq1_dev";
-         $password = "Csci213+#006";
-         $dbname = "steverq1_devdb";
+        $servername = "localhost";
+        $username = "steverq1_dev";
+        $password = "Csci213+#006";
+        $dbname = "steverq1_devdb";
          
         $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -20,12 +20,14 @@
         $result = $conn->query($sql); 
 
         if($result->num_rows > 0) {
+            echo "<table><tr><th>ID</th><th>Name</th></tr>"; 
             //output data of each row
             while($row = $result->fetch_assoc()) {
-                echo "id: " . $row["cust_id"]. " - Name: ". 
-                $row["cust_fname "]. " " . $row["cust_lname"]. 
-                "email: " . $row["cust_email"]. "<br>"; 
+                echo "<tr><td>" . $row["cust_id"]. "</td><td". 
+                $row["cust_fname "]. " " . $row["cust_lname"]. "</td><td>". 
+                "email: " . $row["cust_email"]. "</td><tr>"; 
             }
+            echo "</table>";
         }else {
             echo "0 results"; 
         }
